20d3f9ad3e2c9816568b7b894c0400a4886195cb
alexey-y-a <lexus2157@mail.ru>
2025-06-12 00:19:23 +0300

    step_023 done

--- src/test/java/org/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ src/test/java/org/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -6,49 +6,49 @@ import org.travel.insurance.rest.TravelCalculatePremiumRequest;
 import java.util.Date;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class TravelCalculatePremiumServiceImplTest {
 
     private DateTimeService dateTimeService;
     private TravelCalculatePremiumServiceImpl travelCalculatePremiumService;
+    private TravelCalculatePremiumRequest travelCalculatePremiumRequest;
 
     @BeforeEach
     public void setUp() {
-        dateTimeService = new DateTimeService();
+        travelCalculatePremiumRequest = createRequestWithAllFields();
+        dateTimeService = mock(DateTimeService.class);
+        when(dateTimeService.getDaysBetween(travelCalculatePremiumRequest.getAgreementDateFrom(), travelCalculatePremiumRequest.getAgreementDateTo())).thenReturn(0L);
         travelCalculatePremiumService = new TravelCalculatePremiumServiceImpl(dateTimeService);
     }
 
     @Test
     public void shouldPopulateFirstName() {
-        var travelCalculatePremiumRequest = createRequestWithAllFields();
         var travelCalculatePremiumResponse = travelCalculatePremiumService.calculatePremium(travelCalculatePremiumRequest);
         assertEquals(travelCalculatePremiumResponse.getPersonFirstName(), travelCalculatePremiumRequest.getPersonFirstName());
     }
 
     @Test
     public void shouldPopulateLastName() {
-        var travelCalculatePremiumRequest = createRequestWithAllFields();
         var travelCalculatePremiumResponse = travelCalculatePremiumService.calculatePremium(travelCalculatePremiumRequest);
         assertEquals(travelCalculatePremiumResponse.getPersonLastName(), travelCalculatePremiumRequest.getPersonLastName());
     }
 
     @Test
     public void shouldPopulateAgreementDateFrom() {
-        var travelCalculatePremiumRequest = createRequestWithAllFields();
         var travelCalculatePremiumResponse = travelCalculatePremiumService.calculatePremium(travelCalculatePremiumRequest);
         assertEquals(travelCalculatePremiumResponse.getAgreementDateFrom(), travelCalculatePremiumRequest.getAgreementDateFrom());
     }
 
     @Test
     public void shouldPopulateAgreementDateTo() {
-        var travelCalculatePremiumRequest = createRequestWithAllFields();
         var travelCalculatePremiumResponse = travelCalculatePremiumService.calculatePremium(travelCalculatePremiumRequest);
         assertEquals(travelCalculatePremiumResponse.getAgreementDateTo(), travelCalculatePremiumRequest.getAgreementDateTo());
     }
 
     @Test
     public void shouldPopulateAgreementPrice() {
-        var travelCalculatePremiumRequest = createRequestWithAllFields();
         var travelCalculatePremiumResponse = travelCalculatePremiumService.calculatePremium(travelCalculatePremiumRequest);
         assertNotNull(travelCalculatePremiumResponse.getAgreementPrice());
     }
