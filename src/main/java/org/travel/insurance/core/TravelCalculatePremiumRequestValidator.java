3ce7de9fb1a218546f51c354daf4798cc8b3a869
alexey-y-a <lexus2157@mail.ru>
2025-06-22 00:07:36 +0300

    step_034 done

--- src/main/java/org/travel/insurance/core/TravelCalculatePremiumRequestValidator.java
+++ src/main/java/org/travel/insurance/core/TravelCalculatePremiumRequestValidator.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 import org.travel.insurance.dto.TravelCalculatePremiumRequest;
 import org.travel.insurance.dto.ValidationError;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,6 +17,7 @@ public class TravelCalculatePremiumRequestValidator {
         validatePersonLastName(travelCalculatePremiumRequest).ifPresent(errors::add);
         validateAgreementDateFrom(travelCalculatePremiumRequest).ifPresent(errors::add);
         validateAgreementDateTo(travelCalculatePremiumRequest).ifPresent(errors::add);
+        validateDateFromLessThenDateTo(travelCalculatePremiumRequest).ifPresent(errors::add);
         return errors;
     }
 
@@ -40,6 +42,15 @@ public class TravelCalculatePremiumRequestValidator {
     private Optional<ValidationError> validateAgreementDateTo(TravelCalculatePremiumRequest travelCalculatePremiumRequest) {
         return (travelCalculatePremiumRequest.getAgreementDateTo() == null)
                 ? Optional.of(new ValidationError("agreementDateTo", "Must not be empty!"))
+                : Optional.empty();
+    }
+
+    private Optional<ValidationError> validateDateFromLessThenDateTo(TravelCalculatePremiumRequest travelCalculatePremiumRequest) {
+        Date dateFrom = travelCalculatePremiumRequest.getAgreementDateFrom();
+        Date dateTo = travelCalculatePremiumRequest.getAgreementDateTo();
+        return (dateFrom != null && dateTo != null
+                && (dateFrom.equals(dateTo) || dateFrom.after(dateTo)))
+                ? Optional.of(new ValidationError("agreementDateFrom", "Must be less then agreementDateTo!"))
                 : Optional.empty();
     }
 
