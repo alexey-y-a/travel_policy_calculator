f540b381e3dce913ec965db130d311a8a7e4468b
alexey-y-a <lexus2157@mail.ru>
2025-06-30 22:59:45 +0300

    step_042 done

--- src/main/java/org/travel/insurance/dto/TravelCalculatePremiumRequest.java
+++ src/main/java/org/travel/insurance/dto/TravelCalculatePremiumRequest.java
@@ -1,5 +1,6 @@
 package org.travel.insurance.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +15,11 @@ public class TravelCalculatePremiumRequest {
 
     private String personFirstName;
     private String personLastName;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date agreementDateFrom;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date agreementDateTo;
 
 }
