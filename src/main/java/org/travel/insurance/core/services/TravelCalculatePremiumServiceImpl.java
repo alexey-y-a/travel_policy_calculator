018ea37d5da5c6f12201826294a59dab1baabc47
alexey-y-a <lexus2157@mail.ru>
2025-08-03 01:34:08 +0300

    step_099 done

--- src/main/java/org/travel/insurance/core/services/TravelCalculatePremiumServiceImpl.java
+++ src/main/java/org/travel/insurance/core/services/TravelCalculatePremiumServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.travel.insurance.core.underwriting.TravelPremiumUnderwriting;
 import org.travel.insurance.core.validations.TravelCalculatePremiumRequestValidator;
+import org.travel.insurance.dto.RiskPremium;
 import org.travel.insurance.dto.TravelCalculatePremiumRequest;
 import org.travel.insurance.dto.TravelCalculatePremiumResponse;
 import org.springframework.stereotype.Component;
@@ -37,8 +38,14 @@ public class TravelCalculatePremiumServiceImpl implements TravelCalculatePremium
         travelCalculatePremiumResponse.setAgreementDateFrom(travelCalculatePremiumRequest.getAgreementDateFrom());
         travelCalculatePremiumResponse.setAgreementDateTo(travelCalculatePremiumRequest.getAgreementDateTo());
         travelCalculatePremiumResponse.setAgreementPremium(premium);
-
+        travelCalculatePremiumResponse.setRisks(buildRisks(travelCalculatePremiumRequest));
         return travelCalculatePremiumResponse;
+    }
+
+    private List<RiskPremium> buildRisks(TravelCalculatePremiumRequest request) {
+        return request.getSelectedRisks().stream()
+                .map(riskIc -> new RiskPremium(riskIc, BigDecimal.ZERO))
+                .toList();
     }
 
 }
